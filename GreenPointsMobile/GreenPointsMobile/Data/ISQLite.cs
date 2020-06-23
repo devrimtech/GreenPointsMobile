f76b84f37fecb70cef4e248b4dae486603d28bb5
Kai Devrim <kkgithub@devrim.tech>
2020-06-22 22:07:35 -0700

    Started work on the database, no updates for a little bit as i have to learn SQL, and Entity Framework to fully implement the backend of the app.

---
+++ GreenPointsMobile/GreenPointsMobile/Data/ISQLite.cs
@@ -0,0 +1,12 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GreenPointsMobile.Data
+{
+    public interface ISQLite
+    {
+        SQLiteConnection GetConnection();
+    }
+}
